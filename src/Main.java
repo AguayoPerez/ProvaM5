39cc4720c36889e7f3615c15cadf7bdd9f9cd8ad
javiperez <>
2015-03-29 19:28:02 +0200

    Commit en la nova branca

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                System.out.println("Codigo maestro");
+            } else {
+                System.out.println("Sigue siendo codigo maestro... pero no tanto");
+            }
+        }
     }
 }
